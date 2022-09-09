2b86caa9f5dc7c74dd2bdbc8b0fe1a102ff7999e
Aleksy <aslepaugo@gmail.com>
2022-09-09 11:52:12 +0200

    removed duration from storage info

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -7,12 +7,11 @@ def storage_information_view(request):
     non_closed_visits = []
 
     for visit in Visit.objects.filter(leaved_at__isnull=True):
-        visit_duration = visit.get_duration()
         non_closed_visits.append({
             'who_entered': visit.passcard,
             'entered_at': visit.entered_at,
-            'duration': visit.format_duration(visit_duration),
-            'is_strange': visit.is_visit_long(visit_duration)
+            'duration': visit.format_duration(),
+            'is_strange': visit.is_visit_long()
         })
 
     context = {
